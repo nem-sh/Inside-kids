9823730cd29786b8ec250e4d8eaea9597f8e5ce6
박인남 <pin954562@gmail.com>
2020-09-14 17:11:34 +0900

    [Feat] add kid create feature, resolves S03P22B106-85

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,16 @@
-from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 # Create your models here.
+
+
 class User(AbstractUser):
     pass
 
+
 class Kid(models.Model):
     name = models.CharField(max_length=50)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='kid')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE, related_name='kid')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Kid
+from .serializers import KidSerializer
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create(request):
+    serializer = KidSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response({"status": "OK", **serializer.data})
