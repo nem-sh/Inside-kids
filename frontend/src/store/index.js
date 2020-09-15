d055b33b60a8804f28c4d859b0527c4814674086
조수미 <tnal1632@naver.com>
2020-09-15 13:44:50 +0900

    [Feat] signup logic test

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+import cookies from "vue-cookies";
+import router from "@/router";
+import axios from "axios";
+
+import SERVER from "@/api/drf";
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    authToken: cookies.get("auth-token"),
+    user: {},
   },
   mutations: {
+    SET_TOKEN(state, token) {
+      state.authToken = token;
+      cookies.set("auth-token", token);
+    },
+    SET_USER(state, userInfo) {
+      state.user = userInfo;
+    },
   },
   actions: {
+    signup({ commit }, signupData) {
+      axios
+        .post(SERVER.URL + SERVER.ROUTES.signup, signupData)
+        .then((res) => {
+          console.log(res.data);
+          commit("SET_TOKEN", res.data);
+          router.push({ name: "KidsDetailView", params: { kidId: 1 } });
+        })
+        .catch((err) => console.log(err.response));
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
