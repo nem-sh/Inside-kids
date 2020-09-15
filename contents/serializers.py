f91edce7b3095e795ae87834ab23eee03ce483c4
남시성 <joyclub2020@naver.com>
2020-09-15 11:04:13 +0900

    [Feat] Video delete, resolves S03P22B106-95

---
+++ contents/serializers.py
@@ -0,0 +1,2 @@
+from rest_framework import serializers
+from .models import Video, Paint, Picture, Music, Script, Character
--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from accounts.models import Kid, User
 
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Video, Paint, Picture, Music, Script, Script, Character
+from .serializers import PaintListSerializer, PictureListSerializer, MusicListSerializer, ScriptSerializer
+
+
+# video
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def video_delete(request, video_id):
+    video = get_object_or_404(Video, pk=video_id)
+    if request.user == video.kid.user:
+        video.delete()
+        return HttpResponse(status=204)
+    else:
+        return HttpResponse(status=403)
