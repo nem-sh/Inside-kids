a0251b3f777d494c899fd94a4385342e1828f5ea
김도연 <kdymay93@naver.com>
2020-09-15 20:28:51 +0900

    [Feat] navebar kidselect, resolves S03P22B106-110

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -14,10 +14,11 @@ export default new Vuex.Store({
     authToken: cookies.get('auth-token'),
     user: {},
     kid: {},
+    kidslist: {},
   },
   getters: {
     isLoggedIn: state => !!state.authToken,
-    config: state => ({ headers: { Authorization: `Token ${state.authToken}` } }),
+    config: state => ({ headers: { Authorization: `jwt ${state.authToken}` } }),
   },
   mutations: {
     SET_TOKEN(state, token) {
@@ -29,6 +30,9 @@ export default new Vuex.Store({
     },
     SET_KID(state, kidInfo) {
       state.kid = kidInfo
+    },
+    SET_KIDSLIST(state, kids) {
+      state.kidslist = kids
     },
   },
   actions: {
@@ -102,7 +106,7 @@ export default new Vuex.Store({
     getKidsList({ getters, commit}) {
       axios.get(SERVER.URL + SERVER.ROUTES.getKidInfo, getters.config)
         .then(res => {
-          commit('SET_KID', res.data)
+          commit('SET_KIDSLIST', res.data)
         })
         .catch(err => { console.error(err) })
       },
