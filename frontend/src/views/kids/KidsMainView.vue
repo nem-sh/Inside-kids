94de0db296b13f52a3be9ad38e410dd5b75fd9a7
남시성 <joyclub2020@naver.com>
2020-09-22 16:25:23 +0900

    [Feat] 4 action apply, resolves S03P23B106-28

--- frontend/src/views/kids/KidsMainView.vue
+++ frontend/src/views/kids/KidsMainView.vue
@@ -1,33 +1,64 @@
 <template>
   <div class="kid-main">
+    <!-- 윗 탭 -->
     <v-col class="text-center my-10">
+      <!-- 그림그리기 -->
       <v-btn color="error mx-15 px-14 py-14" fab x-large dark>
         <i class="fa fa-image fa-4x"></i>
       </v-btn>
-
+      <!-- 사진찍기 -->
       <v-btn color="cyan mx-15 px-14 py-14" fab x-large dark>
         <i class="fa fa-camera fa-4x"></i>
       </v-btn>
-
+      <!-- 동요부르기 -->
       <v-btn color="green mx-15 px-14 py-14" fab x-large dark>
         <i class="fa fa-music fa-4x"></i>
       </v-btn>
     </v-col>
 
+    <!-- 캐릭터 -->
     <v-col class="text-center my-10">
-      <img class="character" src="../../assets/character3.png" height="300px" alt="character" />
+      <!-- 기본 -->
+      <img
+        v-if="!actionOnOff"
+        class="character"
+        src="../../assets/characters/character.png"
+        height="240px"
+        width="150px"
+        alt="character"
+      />
+      <!-- 행동 -->
+      <div v-else class="action-box">
+        <div :class="doAction"></div>
+      </div>
     </v-col>
 
+    <!-- 아랫 탭 -->
     <v-col class="text-center my-10">
-      <v-btn color="error mx-15 px-14 py-14" fab x-large dark>
+      <!-- 식사하기 -->
+      <v-btn
+        @click="actionNum = 1; actionOnOff = true;"
+        color="error mx-15 px-14 py-14"
+        fab
+        x-large
+        dark
+      >
         <i class="fa fa-utensils fa-4x"></i>
       </v-btn>
 
+      <!-- 대화하기 -->
       <v-btn color="cyan mx-15 px-14 py-14" fab x-large dark>
         <i class="fa fa-phone fa-4x"></i>
       </v-btn>
 
-      <v-btn color="green mx-15 px-14 py-14" fab x-large dark>
+      <!-- 씻기 -->
+      <v-btn
+        @click="actionNum = 2; actionOnOff = true;"
+        color="green mx-15 px-14 py-14"
+        fab
+        x-large
+        dark
+      >
         <i class="fa fa-shower fa-4x"></i>
       </v-btn>
     </v-col>
@@ -37,6 +68,61 @@
 <script>
 export default {
   name: "KidsMainView",
+  data: function () {
+    return {
+      actionOnOff: false,
+      actionNum: 0,
+      actionCnt: 0,
+    };
+  },
+  methods: {
+    characterNonActionAlgo: function () {
+      let cnt = this.actionCnt;
+      let nonActionList = [3, 4];
+      let nonActionNum =
+        nonActionList[Math.floor(Math.random() * nonActionList.length)];
+      let rand = Math.random() * (10000 - 3000) + 3000;
+      setTimeout(() => {
+        if (cnt == this.actionCnt) {
+          this.actionOnOff = true;
+          this.actionNum = nonActionNum;
+        }
+      }, rand);
+    },
+    actionTimer: function (ms) {
+      setTimeout(() => {
+        this.actionOnOff = false;
+        this.actionNum = 0;
+        this.characterNonActionAlgo();
+      }, ms);
+    },
+    wasAction: function () {
+      this.actionCnt += 1;
+    },
+  },
+  computed: {
+    doAction: function () {
+      if (this.actionNum == 0) {
+        return "non-action";
+      } else {
+        this.wasAction();
+        this.actionTimer(3000);
+        if (this.actionNum == 1) {
+          return "eating";
+        } else if (this.actionNum == 2) {
+          return "washing";
+        } else if (this.actionNum == 3) {
+          return "greeting";
+        } else if (this.actionNum == 4) {
+          return "heart-dancing";
+        }
+      }
+      return "non-action";
+    },
+  },
+  created: function () {
+    this.characterNonActionAlgo();
+  },
 };
 </script>
 
