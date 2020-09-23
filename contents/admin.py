b78b1baa6ac74b199edae3d794c6ce25be8492e8
박인남 <pin954562@gmail.com>
2020-09-23 17:56:43 +0900

    [Feat] change kid detail , resolves S03P23B106-35

--- contents/admin.py
+++ contents/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Paint, Picture, Video
+from .models import Paint, Picture, Video, Script
 
 admin.site.register(Paint)
 admin.site.register(Picture)
 admin.site.register(Video)
+admin.site.register(Script)
--- contents/views.py
+++ contents/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Video, Paint, Picture, Music, Script, Script, Character
+from .models import Video, Paint, Picture, Music, Script, Character
 from .serializers import VideoSerializer, PaintSerializer, PaintListSerializer, PictureSerializer, PictureListSerializer, MusicListSerializer, ScriptSerializer, CharacterSerializer
 
 
