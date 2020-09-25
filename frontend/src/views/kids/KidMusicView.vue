3de39bafd897c1f0ee47087caf30723b6e4fb2a6
남시성 <joyclub2020@naver.com>
2020-09-25 11:51:31 +0900

    [Feat] add music and hungry sound
    Former-commit-id: 497112d1a0c3bb30b24087b783727150b0931fbb

--- frontend/src/views/kids/KidMusicView.vue
+++ frontend/src/views/kids/KidMusicView.vue
@@ -28,12 +28,15 @@
           align-items: center;
           flex-direction: column;
         "
-      >
-        <v-btn @click="start" class="blue" v-if="!isStart">
+        ><v-btn @click="back" class="blue" v-if="!isStart"
+          ><i class="fas fa-home"></i>
+        </v-btn>
+        <v-btn @click="music1" class="green mt-4" v-if="!isStart" width="140">
           싹이 싹이 나네요 ♪
         </v-btn>
-
-        <v-btn @click="back" class="red mt-4" v-if="!isStart"> 뒤로 </v-btn>
+        <v-btn @click="music2" class="orange mt-4" v-if="!isStart" width="140">
+          하루 ♪
+        </v-btn>
 
         <div>
           <h3 style="text-align: center">{{ text1 }}</h3>
@@ -60,6 +63,8 @@
     </v-row>
 
     <audio id="1" src="../../assets/music/1.wav"></audio>
+
+    <audio id="2" src="../../assets/music/2.mp3"></audio>
   </div>
 </template>
 
@@ -74,10 +79,13 @@ export default {
     };
   },
   methods: {
-    start: function () {
+    music1: function () {
       this.isStart = true;
       var audio = document.getElementById("1");
       audio.play();
+      this.text1 = "싹이 싹이 나네요 ♪";
+
+      this.text2 = "";
       setTimeout(() => {
         this.text1 = "싹이 싹이 나네요 작은 새싹이나네요.";
         this.text2 = "둥근둥근 씨앗이 이쁜 새싹이 되어요.";
@@ -107,6 +115,57 @@ export default {
         this.text2 = "";
         this.isStart = false;
       }, 70000);
+    },
+    music2: function () {
+      this.isStart = true;
+      var audio = document.getElementById("2");
+      audio.play();
+      this.text1 = "하루 ♪";
+      this.text2 = "";
+      setTimeout(() => {
+        this.text1 = "해가 오면 날이 밝아";
+        this.text2 = "아침이 되고";
+      }, 5000);
+      setTimeout(() => {
+        this.text1 = "낮이되면 해가 ";
+        this.text2 = "세상 가득 비추죠";
+      }, 12000);
+      setTimeout(() => {
+        this.text1 = "붉게 물든 하늘도";
+        this.text2 = "해가 떠나면";
+      }, 20000);
+      setTimeout(() => {
+        this.text1 = "까만하늘위에";
+        this.text2 = "달이 찾아오지요";
+      }, 26000);
+      setTimeout(() => {
+        this.text1 = "전주 ♪";
+        this.text2 = "";
+      }, 35000);
+      setTimeout(() => {
+        this.text1 = "해가 오면 날이 밝아";
+        this.text2 = "아침이 되고";
+      }, 40000);
+      setTimeout(() => {
+        this.text1 = "낮이되면 해가 ";
+        this.text2 = "세상 가득 비추죠";
+      }, 47000);
+      setTimeout(() => {
+        this.text1 = "붉게 물든 하늘도";
+        this.text2 = "해가 떠나면";
+      }, 55000);
+      setTimeout(() => {
+        this.text1 = "까만하늘위에";
+        this.text2 = "달이 찾아오지요";
+      }, 70000);
+      setTimeout(() => {
+        this.text1 = "";
+        this.text2 = "";
+        this.isStart = false;
+      }, 70000);
+    },
+    back: function () {
+      this.$router.push(`/child/${this.$route.params.kidId}`);
     },
   },
   computed: {},
--- frontend/src/views/kids/KidsMainView.vue
+++ frontend/src/views/kids/KidsMainView.vue
@@ -119,6 +119,10 @@
         id="wash-sound"
         src="../../assets/characterSounds/washing.mp3"
       ></audio>
+      <audio
+        id="hungry-sound"
+        src="../../assets/characterSounds/hungry.mp3"
+      ></audio>
     </div>
   </div>
 </template>
@@ -193,8 +197,11 @@ export default {
     eat: function () {
       this.actionNum = 1;
       this.actionOnOff = true;
-      var audio = document.getElementById("eat-sound");
-      audio.play();
+      this.hungry = false;
+      var audio1 = document.getElementById("hungry-sound");
+      audio1.pause();
+      var audio2 = document.getElementById("eat-sound");
+      audio2.play();
       axios
         .put(
           SERVER.URL +
@@ -206,7 +213,7 @@ export default {
         )
         .then(() => {
           // this.SET_CHARACTER(null);
-          this.hungry = false;
+
           this.getCharacter(this.$route.params.kidId);
         })
         .catch((err) => {
@@ -239,6 +246,15 @@ export default {
           console.error(err);
         });
     },
+    hungrySoundOn: function () {
+      if (this.hungry) {
+        var audio = document.getElementById("hungry-sound");
+        audio.play();
+        setTimeout(() => {
+          this.hungrySoundOn();
+        }, 10000);
+      }
+    },
   },
   computed: {
     ...mapState(["character"]),
@@ -246,6 +262,7 @@ export default {
     doAction: function () {
       if (this.actionNum == 0) {
         if (this.hungry) {
+          this.hungrySoundOn();
           return "hungry";
         } else if (this.dirty) {
           return "dirty";
