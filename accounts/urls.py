663fd785dbed64bb5a834c9298ef5ab347c46914
박인남 <pin954562@gmail.com>
2020-09-28 18:00:00 +0900

    [Feat] verify email, resolves S03P23B106-75
    Former-commit-id: d610641ec9e629abfcd4a4dbfd24110a36d7e57b

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('delete/', views.delete),
     path('kids/', views.kid_create_or_list),
     path('kids/<int:kid_id>/', views.kid_detail_or_update_or_delete),
-    path('google/', views.GoogleLogin.as_view())
+    path('google/', views.GoogleLogin.as_view()),
 ]
