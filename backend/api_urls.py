923dcc0ddbc12fe66feecb5a5a22559c8e739043
박인남 <pin954562@gmail.com>
2020-09-28 18:34:01 +0900

    [Fix] bug fix api_url.py
    Former-commit-id: 43c16161702aec777872a39a2d50d4fdd617c5f9

--- backend/api_urls.py
+++ backend/api_urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
          accounts_views.email_verification,
          name="account_confirm_email"),
     path('accounts/signup/', include('rest_auth.registration.urls')),
+    re_path(r'^account-confirm-email/', VerifyEmailView.as_view(),
+            name='account_email_verification_sent'),
     path('accounts/password/reset/',
          views.PasswordResetView.as_view(), name="password_reset"),
     re_path(r'accounts/password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
