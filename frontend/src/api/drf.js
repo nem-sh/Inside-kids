097c83f223438f3e34f57cb74ac8cd4f048e0294
조수미 <tnal1632@naver.com>
2020-09-30 19:11:59 +0900

    [Fix] drf.js
    Former-commit-id: 1e87b17ab7f69c06c73ec8c6db43806a74683c6b

--- frontend/src/api/drf.js
+++ frontend/src/api/drf.js
@@ -3,18 +3,18 @@ export default {
   // URL: "https://j3b106.p.ssafy.io",
   ROUTES: {
     // auth
-    login: "/api/accounts/login/",
-    logout: "/api/accounts/logout/",
-    signup: "/api/accounts/signup/",
-    resetPwd: "/api/accounts/password/reset/",
-    getUserInfo: "/api/accounts/user/",
-    editUserInfo: "/api/accounts/user/",
-    passwordChange: "/api/accounts/password/change/",
-    deletAccount: "/api/accounts/delete/",
-    getKidInfo: "/api/accounts/kids/",
+    login: "/accounts/login/",
+    logout: "/accounts/logout/",
+    signup: "/accounts/signup/",
+    resetPwd: "/accounts/password/reset/",
+    getUserInfo: "/accounts/user/",
+    editUserInfo: "/accounts/user/",
+    passwordChange: "/accounts/password/change/",
+    deletAccount: "/accounts/delete/",
+    getKidInfo: "/accounts/kids/",
 
     // contents
-    getCharacterInfo: "/api/contents/characters/",
-    deleteVideo: "/api/contents/videos/",
+    getCharacterInfo: "/contents/characters/",
+    deleteVideo: "/contents/videos/",
   },
 };
