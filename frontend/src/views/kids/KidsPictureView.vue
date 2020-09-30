3364acece06ed152f52d1648c3d922aada3b72be
김도연 <kdymay93@naver.com>
2020-09-30 10:42:15 +0900

    [Feat] take picture
    Former-commit-id: 0de40b56ebc4ef215c2ebda862a88d1f4953bada

--- frontend/src/views/kids/KidsPictureView.vue
+++ frontend/src/views/kids/KidsPictureView.vue
@@ -3,6 +3,20 @@
     class="kid-main"
     style="display: flex; justify-content: center; align-items: center"
   >
+    <div class="controls">
+      <div>
+        <img
+          class="back-btn"
+          @click="gokidhome"
+          src="../../assets/icons/back.png"
+          alt="back_btn"
+          width="80px"
+        />
+        <!-- <v-btn @click="gokidhome" class="red mb-2">
+          <i class="fas fa-home fa-2x"></i>
+        </v-btn>-->
+      </div>
+      </div>
     <KidsPicture />
   </div>
 </template>
@@ -13,6 +27,14 @@ export default {
   name: "KidsPictureView",
   components: {
     KidsPicture,
+  },
+  methods:{
+    gokidhome() {
+      this.$router.push({
+        name: "KidsMainView",
+        params: { kidId: 1 },
+      });
+    },
   },
 };
 </script>
