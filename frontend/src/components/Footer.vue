f942ad2579faec8b6b8d4d8592d43a6e11eb6136
김도연 <kdymay93@naver.com>
2020-10-01 21:03:38 +0900

    [Style] landing page2
    Former-commit-id: 7d812f34853f1e44a889f063f5b04e3345a8c7e7

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <v-footer padless>
-      <v-col class="text-center indigo lighten-3" cols="12">
-        <strong>Inside Kids</strong>
+      <v-col class="text-center grey lighten-3" cols="12">
+        <p>대표자 박인남 | 상호명 주식회사 인사이드키즈 | 대전광역시 유성구 덕명동 1234-5678</p>
+        <pre>대표전화 070 1234 5678 | 사업자등록번호 535-87-0000234</pre>
+        <pre>통신판매신고번호 2222-대전유성-3009 | 개인정보관리책임자 조수미</pre>
+        <strong>Copyright 2020 (주) 인사이드키즈</strong>
       </v-col>
     </v-footer>
   </div>
