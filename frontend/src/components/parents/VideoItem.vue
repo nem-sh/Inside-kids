52d94720581a96c7ea472c7106b10d262a8a4e40
남시성 <joyclub2020@naver.com>
2020-10-07 16:48:38 +0900

    lie detect front update, S03P23B106-115
    Former-commit-id: 06be3766a120c8da7582eee188039edb0973c0cb

--- frontend/src/components/parents/VideoItem.vue
+++ frontend/src/components/parents/VideoItem.vue
@@ -8,15 +8,15 @@
     </v-expansion-panel-header>
     <v-expansion-panel-content>
       <video :src="videoUrl" controls width="100%" height="400px"></video>
-      <div style="display: flex; justify-content: space-between" class="mt-5">
+      <div style="display: flex; justify-content: center" class="mt-5">
         <p v-if="lieDetectResult" style="line-height: 36px">
           아이가 <b :class="resultColor">{{ lieDetectResult }}</b> 답변하고
           있어요.
         </p>
         <p v-else>영상을 분석하고 있어요.</p>
-        <div class="text-right">
-          <v-btn @click="deleteVideo">삭제</v-btn>
-        </div>
+      </div>
+      <div class="text-right">
+        <a style="color: red" @click="deleteVideo">삭제</a>
       </div>
     </v-expansion-panel-content>
   </v-expansion-panel>
